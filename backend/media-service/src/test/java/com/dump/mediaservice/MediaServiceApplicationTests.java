3ee91b150a9337ab3f12f288326b24495413af47
Samir Sarwar <samirsarwaremail@gmail.com>
2026-03-22 14:01:38 -0400

    init: backend initialize

---
+++ backend/media-service/src/test/java/com/dump/mediaservice/MediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dump.mediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
